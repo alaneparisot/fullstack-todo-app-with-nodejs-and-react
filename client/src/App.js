f024e2c2b65bd2a61d2929b93d6034fdbb99c4c2
Alane Parisot <27372097+alaneparisot@users.noreply.github.com>
2018-11-10 16:11:16 +0100

    Add Client Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+import Navbar from "./components/Navbar";
+
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Todo App</h1>
+        <Navbar/>
       </div>
     );
   }
